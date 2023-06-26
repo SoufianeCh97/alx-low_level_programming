23b9c0fb3531266db1201803e62be6547d974e3d
Soufiane <soufiane.2013chahid@gmail.com>
2023-06-26 19:31:31 +0100

    Fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,8 +12,8 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		putchar(*str + 0);
+		_putchar(*str + 0);
 		str++;
 	}
-	putchar('\0');
+	_putchar('\0');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,4 +15,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
-
